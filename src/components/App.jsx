ef698209b7f9053adde30cde2921070e1a91d065
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2022-08-29 10:00:58 +0300

    add correct const load
    add correct const load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,8 +49,7 @@ export const App = () => {
         }
     };
 
-
-    const loadMore = () => setPage(prevPage => prevPage + 1);
+    const loadMore = () => setPage(page + 1);
 
     const openModal = (modalContent) => {
         setModalOpen(true);
